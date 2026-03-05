a08d3b8796bdba3008241c20eaf007e43046723e
aphiwe-citiq <aphiwem@citiqprepaid.co.za>
2026-03-05 16:56:26 +0200

    SETUP: Firebase initialisation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+  ],
 };
---
+++ src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'PROD_API_KEY',
+    authDomain: 'PROD_AUTH_DOMAIN',
+    projectId: 'PROD_PROJECT_ID',
+    storageBucket: 'PROD_STORAGE_BUCKET',
+    messagingSenderId: 'PROD_MESSAGING_SENDER_ID',
+    appId: 'PROD_APP_ID',
+    measurementId: 'PROD_MEASUREMENT_ID',
+  },
+};
