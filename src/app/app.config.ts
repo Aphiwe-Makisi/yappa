22cb3ab89b237650cb25ab01c45ee0adf1d33167
aphiwe-citiq <aphiwem@citiqprepaid.co.za>
2026-03-06 14:34:29 +0200

    Theme update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,33 @@ import { routes } from './app.routes';
 import { environment } from '../environments/environment';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#e0f2f1',
+      100: '#b2dfdb',
+      200: '#80cbc4',
+      300: '#4db6ac',
+      400: '#26a69a',
+      500: '#008080', // teal - your primary color
+      600: '#00796b',
+      700: '#00695c',
+      800: '#004d40',
+      900: '#003d33',
+      950: '#002a23',
+    },
+    colorScheme: {
+      light: {
+        surface: {
+          0: '#ffffff',
+        },
+      },
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
