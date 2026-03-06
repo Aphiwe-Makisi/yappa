8a09d109a7841ffd1f013c512b7205a16b290104
aphiwe-citiq <aphiwem@citiqprepaid.co.za>
2026-03-06 14:21:24 +0200

    WIP: signup screen UI fixes and styles

--- src/app/features/auth/pages/signup/signup.ts
+++ src/app/features/auth/pages/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../../../core/services/auth';
 import { sanitisedUserInput } from '../../../../shared/utils';
 import { InputTextModule } from 'primeng/inputtext';
@@ -7,9 +7,19 @@ import { PasswordModule } from 'primeng/password';
 import { IftaLabelModule } from 'primeng/iftalabel';
 import { ButtonModule } from 'primeng/button';
 import { handleFirebaseAuthError } from '../../../../shared/firebase-errors';
+import { RouterLink } from '@angular/router';
+import { User } from '@angular/fire/auth';
 @Component({
   selector: 'app-signup',
-  imports: [ReactiveFormsModule, InputTextModule, PasswordModule, IftaLabelModule, ButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    IftaLabelModule,
+    ButtonModule,
+    FormsModule,
+    RouterLink,
+  ],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
@@ -26,18 +36,21 @@ export class Signup {
 
   initForm(): void {
     this.form = this.fb.group({
+      full_name: [''],
       email: [''],
       password: [''],
+      confirm_password: [''],
     });
   }
 
   register(): any {
+    const fullname = sanitisedUserInput(this.form.get('full_name')?.value);
     const email = sanitisedUserInput(this.form.get('email')?.value);
     const password = sanitisedUserInput(this.form.get('password')?.value);
     this.authService
       .signUp(email, password)
       .then((res: any) => {
-        console.log(res);
+        console.log(res.user);
       })
       .catch((error: any) => {
         this.errorMessage = handleFirebaseAuthError(error.code);
